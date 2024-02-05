57081d15d8f66d9cff398789c2273f2b9a1774a8
Izzat Nasrun <16971676+zatnas@users.noreply.github.com>
2024-02-05 22:39:25 +0800

    grids and more numbers

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -2,8 +2,17 @@ import Button from './button';
 
 export default function Calculator() {
   return (
-    <div className="grid">
+    <div className="grid grid-cols-3 gap-2">
       <Button text="1"/>
+      <Button text="2"/>
+      <Button text="3"/>
+      <Button text="4"/>
+      <Button text="5"/>
+      <Button text="6"/>
+      <Button text="7"/>
+      <Button text="8"/>
+      <Button text="9"/>
+      <Button className="col-span-3" text="0"/>
     </div>
   )
 }
